44da24e784f8fb516232978f4eaf6bd931625595
David Marques <54871078+CCG-web@users.noreply.github.com>
2022-10-14 19:41:07 -0300

    Add files via upload

---
+++ src/main/java/com/company/nomeprojeto/tarefas/dto/TarefaDTO.java
@@ -0,0 +1,60 @@
+package com.company.nomeprojeto.tarefas.dto;
+
+public class TarefaDTO {
+    private String category;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    private String description;
+    private Long id;
+    private String image;
+    private int price;
+    private String title;
+
+}
